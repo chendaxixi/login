c9a21c4017b7ea1608ca1fa413af2fbbb027dcad
chendaxixi <215bcsb342@gmail.com>
2017-11-10 16:02:17 +0800

    update login.py

---
+++ login.py
@@ -0,0 +1,26 @@
+import urllib
+import urllib2
+requrl = "https://net.tsinghua.edu.cn/do_login.php"
+headers = {
+    "Host": "net.tsinghua.edu.cn",
+    "Connection": "Keep-Alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Accept-Language": "zh-CN,zh;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Referer": "https://net.tsinghua.edu.cn/wireless/",
+    "Origin": "https://net.tsinghua.edu.cn",
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    }
+data = {
+    "action": "login",
+    "username": "用户名",
+#    "password": "{MD5_HEX}b806885273e7a4df76e71cca1a0fdcc4",
+    "password": "{MD5_HEX}密码的md5加密",
+    "ac_id": "1"
+    }
+data_urlencode = urllib.urlencode(data)
+req = urllib2.Request(url=requrl, data=data_urlencode, headers=headers)
+res_data = urllib2.urlopen(req)
+print res_data.read()
